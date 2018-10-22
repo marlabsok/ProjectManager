6937785bdd27575ffa7c7d30b2846748ee8fdd26
marlabsok <43975937+marlabsok@users.noreply.github.com>
2018-10-23 00:28:38 +0300

    Подключен Фрагмент на главном экране

--- feature/src/main/java/com/comfboard/notebook/feature/MainActivity.kt
+++ feature/src/main/java/com/comfboard/notebook/feature/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comfboard.notebook.feature
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -10,26 +11,28 @@ import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
-    private var mToolbar: Toolbar? = null
+    private var mFm = supportFragmentManager
+    private var mFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        mToolbar = findViewById<Toolbar>(R.id.toolbar)
-//        setActionBar(mToolbar)
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-        var bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
-        bottomBar!!.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener{
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when(item.itemId){
-                    R.id.new_project -> Toast.makeText(this@MainActivity, R.string.menu_new_project, Toast.LENGTH_SHORT).show()
-                }
-                return false
+        bottomBar!!.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.new_project -> Toast.makeText(this@MainActivity, R.string.menu_new_project, Toast.LENGTH_SHORT).show()
             }
+            false
+        }
 
-        })
+        mFragment = mFm.findFragmentById(R.id.fragmentContainer)
+        if (mFragment == null) {
+            mFragment = ProjectListFragment()
+            mFm.beginTransaction()
+                .add(R.id.fragmentContainer, this.mFragment as ProjectListFragment)
+                .commit()
 
         // TODO: Подключить Toolbar
         // TODO: Подключить Фрагмент-
@@ -38,21 +41,22 @@ class MainActivity : AppCompatActivity() {
         // TODO: Переход в активность Проекта-
     }
 
-    //Меню создано
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.toolbar_user, menu)
-        return true
-    }
-
-    ////Меню выбрано
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val action = item.itemId
-        when (action) {
-            R.id.user -> {
-                Toast.makeText(this, R.string.menu_user, Toast.LENGTH_SHORT).show()
-            }
-            else -> return false
+        //Меню создано
+        fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.toolbar_user, menu)
+            return true
         }
-        return true
+
+        ////Меню выбрано
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+            val action = item.itemId
+            when (action) {
+                R.id.user -> {
+                    Toast.makeText(this@MainActivity, R.string.menu_user, Toast.LENGTH_SHORT).show()
+                }
+                else -> return false
+            }
+            return true
+        }
     }
 }
