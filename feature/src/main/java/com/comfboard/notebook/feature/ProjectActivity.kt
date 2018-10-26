c7c8a0a7465b028f5b0a1dff4a91e4b53122de82
marlabsok <43975937+marlabsok@users.noreply.github.com>
2018-10-26 17:16:35 +0300

    Новая активность и меню

--- feature/src/main/java/com/comfboard/notebook/feature/ProjectActivity.kt
+++ feature/src/main/java/com/comfboard/notebook/feature/ProjectActivity.kt
@@ -7,22 +7,21 @@ import android.support.v4.app.Fragment
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import android.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class ProjectActivity : AppCompatActivity() {
 
     private var mFm = supportFragmentManager
     private var mFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_project)
 
         val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         bottomBar!!.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
-                R.id.new_project -> Toast.makeText(this@MainActivity, R.string.menu_new_project, Toast.LENGTH_SHORT).show()
+                R.id.meta -> Toast.makeText(this@ProjectActivity, R.string.menu_new_project, Toast.LENGTH_SHORT).show()
             }
             false
         }
@@ -33,28 +32,6 @@ class MainActivity : AppCompatActivity() {
             mFm.beginTransaction()
                 .add(R.id.fragmentContainer, this.mFragment as ProjectListFragment)
                 .commit()
-
-        // TODO: Подключить Toolbar
-        // TODO: Подключить Меню
-        // TODO: Переход в активность Проекта
-    }
-
-        //Меню создано
-        fun onCreateOptionsMenu(menu: Menu): Boolean {
-            menuInflater.inflate(R.menu.toolbar_user, menu)
-            return true
-        }
-
-        ////Меню выбрано
-        fun onOptionsItemSelected(item: MenuItem): Boolean {
-            val action = item.itemId
-            when (action) {
-                R.id.user -> {
-                    Toast.makeText(this@MainActivity, R.string.menu_user, Toast.LENGTH_SHORT).show()
-                }
-                else -> return false
-            }
-            return true
         }
     }
 }
